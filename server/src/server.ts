f6ddceeada66368fef258c7732ded471320bd3bb
Elliot <unforriven@gmail.com>
2025-01-27 10:16:07 -0300

    Implementación CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db'
 import solicitudRoutes from './routes/solicitudRoutes';
+import cors from 'cors';
 
 dotenv.config()
 
@@ -9,6 +10,13 @@ connectDB();
 
 // Instancia del servidor
 const app = express()
+
+// Configurar CORS
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 // Habilitar la lectura del formato json para que pueda leer los valores que le envíe desde el body
 app.use(express.json());
